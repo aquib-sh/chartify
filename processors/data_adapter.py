0cd4bbfc96ed6a26163b5f9e0b587dc2bc58a73f
Aquib <shaikhaquib394@gmail.com>
2021-06-13 18:23:43 +0530

    Adds DataAdapter to transfer values between windows/widgets.

---
+++ processors/data_adapter.py
@@ -0,0 +1,18 @@
+class DataAdapter:
+    """Used for exchanging data between different objects and windows."""
+    def __init__(self):
+        self._dict = {}
+
+    def insert(self, _key, _value):
+        self._dict[_key] = _value
+
+    def get(self, _key):
+        if _key not in self._dict : raise KeyError(f"{_key} not found")
+        return self._dict[_key]
+
+    def delete(self, _key):
+        if _key not in self._dict: raise KeyError(f"{_key} not found")
+        del self._dict[_key]
+
+    def keys(self):
+        return tuple(self._dict.keys())
