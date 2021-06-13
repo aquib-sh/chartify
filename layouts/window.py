a15fbf32ab8505c0ab2c4c22a508934ade52d09f
Aquib <shaikhaquib394@gmail.com>
2021-06-13 18:13:49 +0530

    Adds self.quit() before self.destroy() Window was unable to close when just using self.destroy() Adding self.quit() before self.destroy() exits from the mainloop before destroying the window.

--- layouts/window.py
+++ layouts/window.py
@@ -16,7 +16,6 @@ class Application(tk.Tk):
     -------
 
     """
-        
     def __init__(self, title:str, size:tuple):
         """
         Parameters
@@ -35,3 +34,38 @@ class Application(tk.Tk):
     def start(self):
         """Displays the window by starting the mainloop."""
         self.mainloop()
+
+    def exit_window(self):
+        self.destroy()
+
+
+class TopLevelWindow(tk.Toplevel):
+    """A class for top level window of the application.
+
+    This class serves as main tkinter window which can contain menus, submenus, charts, etc.
+
+    Methods
+    -------
+    """
+    def __init__(self, title: str, size: tuple):
+        """
+        Parameters
+        ----------
+        title : str
+            Title of the window
+
+        size : tuple
+            Size of window (x:int, y:int) where x is width and y is height.
+        """
+        super().__init__()
+        self.geometry(f"{size[0]}x{size[1]}")
+        self.resizable(False, False)
+        self.title(title)
+
+    def start(self):
+        """Displays the window by starting the mainloop."""
+        self.mainloop()
+
+    def exit_window(self):
+        self.quit()
+        self.destroy()
