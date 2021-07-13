c23affd52a6ef250748a8ff00ab313be90640f74
Aquib <shaikhaquib394@gmail.com>
2021-07-13 15:00:59 +0530

    Add insert row, insert column.

--- menus/menubar.py
+++ menus/menubar.py
@@ -89,38 +89,38 @@ class MenuBarExtended(tk.Menu):
 
         # Add the first 3 menus
         filemenu  = tk.Menu(self, tearoff=0)
-        #editmenu  = tk.Menu(self, tearoff=0)
+        editmenu  = tk.Menu(self, tearoff=0)
         chartmenu = tk.Menu(self, tearoff=0)
         toolsmenu = tk.Menu(self, tearoff=0)
         optionsmenu = tk.Menu(self, tearoff=0)
 
         # Give names to above menus
         self.add_cascade(label="File", menu=filemenu)
-        #self.add_cascade(label="Edit", menu=editmenu)
+        self.add_cascade(label="Edit", menu=editmenu)
         self.add_cascade(label="3D Chart", menu=chartmenu)
         self.add_cascade(label="Tools", menu=toolsmenu)
         self.add_cascade(label="Options", menu=optionsmenu)
         
-        # SubMenu inside editmenu
-        # insertmenu = tk.Menu(editmenu, tearoff=0)
-        # deletemenu = tk.Menu(editmenu, tearoff=0)
-        # clearmenu  = tk.Menu(editmenu, tearoff=0)
+        #SubMenu inside editmenu
+        insertmenu = tk.Menu(editmenu, tearoff=0)
+        deletemenu = tk.Menu(editmenu, tearoff=0)
+        clearmenu  = tk.Menu(editmenu, tearoff=0)
 
-        # Add labels to the above submenus inside edit menu
-        # editmenu.add_cascade(label="Insert", menu=insertmenu)
-        # editmenu.add_cascade(label="Delete", menu=deletemenu)
-        # editmenu.add_cascade(label="Clear", menu=clearmenu)
+        #Add labels to the above submenus inside edit menu
+        editmenu.add_cascade(label="Insert", menu=insertmenu)
+        editmenu.add_cascade(label="Delete", menu=deletemenu)
+        editmenu.add_cascade(label="Clear", menu=clearmenu)
 
-        # Row, column options for insert, delete and clear
+        #Row, column options for insert, delete and clear
 
-        # insertmenu.add_command(label="Row", command=None)
-        # insertmenu.add_command(label="Column", command=self.master.insert_new_column)
+        insertmenu.add_command(label="Row", command=self.master.insert_row)
+        insertmenu.add_command(label="Column", command=self.master.insert_new_column)
         
-        # deletemenu.add_command(label="Row", command=None)
-        # deletemenu.add_command(label="Column", command=self.master.delete_column)
+        deletemenu.add_command(label="Row", command=None)
+        deletemenu.add_command(label="Column", command=self.master.delete_column)
 
-        # clearmenu.add_command(label="Row", command=None)
-        # clearmenu.add_command(label="Column", command=None)
+        clearmenu.add_command(label="Row", command=None)
+        clearmenu.add_command(label="Column", command=None)
 
         filemenu.add_command(label="Open", command=self.master.open_file)
         filemenu.add_command(label="Save", command=self.master.save_file)
