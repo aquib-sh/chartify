bb3e4365c2924062cdf28133bdd1346c535f2613
Aquib <shaikhaquib394@gmail.com>
2021-07-23 22:15:14 +0530

    Add sqlite database methods for custom colors.

--- processors/cache_memory.py
+++ processors/cache_memory.py
@@ -5,23 +5,23 @@ import json
 class CacheProcessor:
     """Saves and retrives the internal cache of program which has data related to file attributes."""
 
-    def __init__(self):
-        self.__cache_dir__ = os.path.abspath(os.path.join(
-            os.path.pardir, 'cache'))
+    def __init__(self, basefile, table):
+        self.__cache_dir__ = 'cache'
         self.__cache_f__ = os.path.abspath(os.path.join(
-            self.__cache_dir__, 'attributes.db'))
+            self.__cache_dir__, basefile))
 
         if not os.path.exists(self.__cache_dir__) : os.mkdir(self.__cache_dir__)
 
-        self.table = "FileAttributes"
+        self.table = table
         self.conn = sqlite3.connect(self.__cache_f__)
+        print(f"[+] Connected to database in {self.__cache_f__}")
         self.cursor = self.conn.cursor()
         self.cursor.execute(f'''CREATE TABLE IF NOT EXISTS {self.table}
-         (FILE TEXT  PRIMARY KEY NOT NULL,
-         OBJECT_COL  TEXT        NOT NULL,
-         SPACE_COL   TEXT        NOT NULL,
-         ST_TIME_COL TEXT        NOT NULL,
-         ND_TIME_COL TEXT        NOT NULL);''')
+            (COLOR TEXT PRIMARY KEY NOT NULL,
+             RED   REAL             NOT NULL,
+             GREEN REAL             NOT NULL,
+             BLUE  REAL             NOT NULL,
+             ALPHA REAL             NOT NULL);''')
         self.conn.commit()
 
 
@@ -34,46 +34,76 @@ class CacheProcessor:
         return True
 
 
-    def insert_cache(self, data:dict):
-        """Inserts a new record into the database."""
+    def insert_cache(self, colorname: str, rgba: tuple[float, float, float, float]):
+        """Inserts a new record into the database.
+        
+        Parameters
+        ----------
+        colorname: str
+            Colorname to save it as.
+
+        data: tuple[float, float, float, float]
+            RGBA float values must be passed as a tuple
+            (red_value, green_value, blue_value, alpha_value)
+        """
+        if colorname == None or type(colorname) != str or len(colorname) <=1:
+            raise Exception("InvalidColorNameError", "colorname must be a string with length > 1")
+        if len(rgba) != 4:
+            raise Exception("InvalidParameterError", 
+                "data: tuple must have only 4 elements, (red_value, green_value, blue_value, alpha_value)")
+
         # Get all the data from dictionary
-        fname = data['file']
-        object_col  = data['object_col']
-        space_col   = data['space_col']
-        st_time_col = data['st_time_col']
-        nd_time_col = data['nd_time_col']
+        red, green, blue, alpha = rgba
         # Insert the above record into database.
-        self.cursor.execute(f"INSERT INTO {self.table} (FILE, OBJECT_COL, SPACE_COL, ST_TIME_COL, ND_TIME_COL) VALUES (?,?,?,?,?)",
-            (fname, object_col, space_col, st_time_col, nd_time_col))
+        self.cursor.execute(f"INSERT INTO {self.table} (COLOR, RED, GREEN, BLUE, ALPHA) VALUES (?,?,?,?,?)",
+            (colorname, red, green, blue, alpha))
         # Commit the changes
         self.conn.commit()
 
 
-    def update_cache(self, data:dict):
-        """Updates the existing cache."""
+    def update_cache(self, colorname: str, rgba: tuple[float, float, float, float]):
+        """Updates the existing cache.
+        
+        Parameters
+        ----------
+        colorname: str
+            Colorname to save it as.
+
+        data: tuple[float, float, float, float]
+            RGBA float values must be passed as a tuple
+            (red_value, green_value, blue_value, alpha_value)
+        """
         # Get all the data from dictionary
-        fname = data['file']
-        object_col  = data['object_col']
-        space_col   = data['space_col']
-        st_time_col = data['st_time_col']
-        nd_time_col = data['nd_time_col']
+        if colorname == None or type(colorname) != str or len(colorname) <=1:
+            raise Exception("InvalidColorNameError", "colorname must be a string with length > 1")
+        if len(rgba) != 4:
+            raise Exception("InvalidParameterError", 
+                "data: tuple must have only 4 elements, (red_value, green_value, blue_value, alpha_value)")
+
+        # Get all the data from dictionary
+        red, green, blue, alpha = rgba
         # Update the database with the above data.
         query = f"""UPDATE {self.table} 
-                    SET OBJECT_COL = ?, 
-                    SPACE_COL = ?, 
-                    ST_TIME_COL = ?, 
-                    ND_TIME_COL = ?
-                WHERE FILE = ?""",
-        self.cursor.execute(query, (object_col, space_col, st_time_col, nd_time_col, fname))
+                    RED = ?, 
+                    GREEN = ?, 
+                    BLUE = ?, 
+                    ALPHA = ?,
+                WHERE COLOR = ?""",
+        self.cursor.execute(query, (colorname, red, green, blue, alpha))
         # Commit the changes
         self.conn.commit()
 
 
-    def retrieve_cache(self, filepath):
-        """Returns the file settings from database."""
-        self.cursor.execute(f"SELECT * FROM {self.table} WHERE FILE={filepath};")
-        got = self.cursor.fetchall()
-        return got
+    def retrieve_cache(self) -> dict:
+        """Returns data from database as a dictionary."""
+        self.cursor.execute(f"SELECT * FROM {self.table};")
+        retrieved_data: list = self.cursor.fetchall()
+        data: dict = {}
+        
+        for row in retrieved_data:
+            data[row[0]] = row[1:]
+
+        return data
 
 
 class CacheSaver:
