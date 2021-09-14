3dd5e528685fd5c9e4f01cce28b57e611cc5ce2f
Aquib <shaikhaquib394@gmail.com>
2021-09-14 16:40:21 +0530

    Imporve formatting.

--- processors/timeline_mapper.py
+++ processors/timeline_mapper.py
@@ -1,27 +1,24 @@
 class TimelineMapper:
     """Maps timeline to actual labels on plot."""
-    
+
     def __init__(self, timeline, ticks):
         self.timeline = timeline
         self.ticks = ticks
         self.map = {}
         self.generate_mappings()
 
-
     def dissect_label(self, label) -> tuple:
         """Returns date, hour, minutes info from given label."""
         date = label.split()[0]
         time_parts = label.split()[1].split(":")
-        hours    = time_parts[0]
+        hours = time_parts[0]
         minutes = time_parts[1]
 
         return (date, int(hours), int(minutes))
 
-
     def generate_mappings(self):
         for i in range(0, len(self.timeline)):
             self.map[self.timeline[i]] = self.ticks[i]
-
 
     def get_all_dates(self) -> list:
         """Returns all the dates."""
@@ -31,18 +28,17 @@ class TimelineMapper:
             dates.append(date)
         return tuple(set(dates))
 
-
-    def get_point(self, search_time:str) -> int:
+    def get_point(self, search_time: str) -> int:
         """Returns the axis point for a given time in the timeline.
-        
+
         Parameters
         ----------
         search_time: str
             Time to search in the mappings for getting axis point.
 
         """
-        stime = self.timeline[0] # start time of the timeline
-        stime_date,  stime_hr,  stime_min  = self.dissect_label(stime)
+        stime = self.timeline[0]  # start time of the timeline
+        stime_date, stime_hr, stime_min = self.dissect_label(stime)
         search_date, search_hr, search_min = self.dissect_label(search_time)
 
         map_keys = list(self.map.keys())
@@ -59,11 +55,7 @@ class TimelineMapper:
                 if (search_hr == map_hr) and (search_min == map_min):
                     return current_point
                 else:
-                    hr_diff  = search_hr  - map_hr
+                    hr_diff = search_hr - map_hr
                     min_diff = search_min - map_min
-                    current_point += (hr_diff*60 + min_diff)
+                    current_point += hr_diff * 60 + min_diff
                     return current_point
-
-
-
-
